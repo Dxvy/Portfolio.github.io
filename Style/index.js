4d17d2ce5227c6a64962e6764dc3379b471fa21d
Davy <davy.marthely@ynov.com>
2023-08-19 08:56:50 +0200

    feat(skill): Complete skill
    Closes #3

--- Style/index.js
+++ Style/index.js
@@ -89,21 +89,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Carousel
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n){
-    var i;
-    var slides = document.getElementsByClassName("carousel-slide");
-    var dots = document.getElementsByClassName("demo");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
